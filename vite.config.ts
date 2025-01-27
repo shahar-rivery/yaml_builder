88979736ed9cc78de51b9c4d829ac497bdfed1dd
Shahar Sprontz <sh.sprontz@gmail.com>
2025-01-27 22:08:09 +0200

    added base

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: '/',
+  base: '/yaml_builder/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
